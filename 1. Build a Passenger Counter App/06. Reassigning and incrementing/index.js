647ca167153f7fae1af6e3cca7483ddae152bd9d
NoahStiasny <156823310+NoahStiasny@users.noreply.github.com>
2025-04-29 12:38:59 +0900

    index.js aktualisieren

--- 1. Build a Passenger Counter App/06. Reassigning and incrementing/index.js
+++ 1. Build a Passenger Counter App/06. Reassigning and incrementing/index.js
@@ -7,4 +7,16 @@ console.log(count)
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
 // Decrease it down to 25, and then finally increase it to 70
 
+let bonusPoints = 50;
+
+bonusPoints = bonusPoints + 50;
+console.log(BonusPoints);
+
+bonusPoints = bonusPoints - 75;
+console.log(BonusPoints);
+
+bonusPoints = bonusPoints + 45;
+console.log(BonusPoints);
+
+
 
